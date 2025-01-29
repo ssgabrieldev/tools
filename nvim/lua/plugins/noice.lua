dee76fd3f0cf4b0ebce937ae0eeb2230a503b6d4
gabriel <ssgabrieldev@gmail.com>
2025-01-29 00:02:53 -0300

    new: nvim

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,16 +3,6 @@ return {
   event = "VeryLazy",
   opts = {
     lsp = {
-      -- progress = {
-      --   enabled = true, -- Ativa as mensagens de progresso do LSP
-      --   format = {
-      --     { "{spinner} ",                  hl_group = "NoiceLspProgressSpinner" },
-      --     { "{data.progress.title} ",      hl_group = "NoiceLspProgressTitle" },
-      --     { "{data.progress.percentage}%", hl_group = "NoiceLspProgressPercentage" },
-      --     { "{data.progress.message}",     hl_group = "NoiceLspProgressMessage" },
-      --   },
-      --   format_done = "✔ {data.progress.title}",
-      -- },
       hover = {
         opts = {
           border = vim.g.border_style,
