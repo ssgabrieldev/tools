91e831ad5db0f274381454e9fc6d87377c74dd73
gabriel <ssgabrieldev@gmail.com>
2025-01-30 19:40:14 -0300

    ref: nvim - dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -4,93 +4,70 @@ local float_opts = {
   border = vim.g.border_style
 }
 
-local ask_webroot = function()
-  return "${workspaceFolder}" .. vim.fn.input("WEB ROOT: ")
-end
-
-local ask_remoteroot = function()
-  return "${workspaceFolder}" .. vim.fn.input("REMOTE ROOT: ")
-end
-
-local ask_url = function()
-  return vim.fn.input("APP URL: ", "http://localhost:3000")
-end
-
-local ask_port = function()
-  return vim.fn.input("BROWSER PORT: ", "9222")
-end
-
-local ask_custom_command = function()
-  local terminal_module = "toggleterm"
-
-  if not package.loaded[terminal_module] then
-    return
-  end
-
-  require("dapui").close()
-
-  local command = vim.fn.input("COMMAND TO RUN: ")
-
-  if command ~= "" then
-    local term = require(terminal_module .. ".terminal").Terminal:new({
-      cmd = command,
-      hidden = false,
-      direction = "float"
-    })
-    term:toggle()
-  end
-end
-
-local got_to_propper_win = function()
-  if
-      string.match(vim.bo.filetype, "dap")
-      or vim.bo.filetype == "toggleterm"
-  then
-    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-      local current_bufnr = vim.bo[bufnr]
-      if vim.fn.bufwinid(bufnr) ~= -1 then
-        if
-            not string.match(current_bufnr.filetype, "dap")
-            and current_bufnr.filetype ~= "toggleterm"
-            and current_bufnr.filetype ~= "NvimTree"
-        then
-          vim.api.nvim_set_current_win(vim.fn.bufwinid(bufnr))
-        end
-      end
-    end
-  end
-end
-
 local M = {
-  "rcarriga/nvim-dap-ui",
+  "mfussenegger/nvim-dap",
   dependencies = {
-    "mfussenegger/nvim-dap",
+    {
+      "rcarriga/nvim-dap-ui",
+      opts = {
+        expand_lines = false
+      }
+    },
     "nvim-neotest/nvim-nio",
     "nvim-telescope/telescope.nvim",
     "nvim-telescope/telescope-dap.nvim",
     'akinsho/toggleterm.nvim',
     "rcarriga/nvim-notify",
   },
+  init = function()
+    local dap = require("dap")
+
+    local js_debugger = { "pwa-chrome", "pwa-node" }
+    for _, debugger in ipairs(js_debugger) do
+      dap.adapters[debugger] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        protocol = "inspector",
+        executable = {
+          command = vim.fn.stdpath("data") .. "/mason/bin/js-debug-adapter",
+          args = { "${port}" },
+        }
+      }
+    end
+
+    dap.listeners.before.attach.dapui_config = function()
+      vim.notify("Debugger attatched", "info")
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      vim.notify("Debugger launched", "info")
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      vim.notify("Debugger teminated", "info")
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      vim.notify("Debugger exited", "info")
+    end
+  end,
   keys = {
     {
       "<leader>db",
       function()
-        vim.cmd("DapToggleBreakpoint")
+        require("dap").toggle_breakpoint()
       end,
       desc = "Debugger toggle breakpoint"
     },
     {
       "<leader>de",
       function()
-        vim.cmd("DapTerminate")
+        require("dap").terminate()
       end,
       desc = "Debugger terminate"
     },
     {
       "<leader>dr",
       function()
-        got_to_propper_win()
-        vim.cmd("DapContinue")
+        require("dap").continue()
       end,
       desc = "Debugger continue"
     },
@@ -104,8 +81,7 @@ local M = {
     {
       "<leader>dv",
       function()
-        require("dapui").eval()
-        require("dapui").eval()
+        require("dapui").eval({ enter = true })
       end,
       mode = { "n", "v" },
       desc = "Debugger evaluate",
@@ -139,100 +115,6 @@ local M = {
       desc = "Debugger list frames"
     },
   },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-
-    local js_debugger = { "pwa-chrome", "pwa-node" }
-    for _, debugger in ipairs(js_debugger) do
-      dap.adapters[debugger] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        protocol = "inspector",
-        executable = {
-          command = vim.fn.stdpath("data") .. "/mason/bin/js-debug-adapter",
-          args = { "${port}" },
-        }
-      }
-    end
-
-    local js = { "javascriptreact", "javascript" }
-    for _, language in ipairs(js) do
-      dap.configurations[language] = {
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Node",
-          runtimeArgs = function()
-            local args = {}
-            local arg = vim.fn.input(
-              "File to run (default: " .. vim.fn.expand('%') .. "): ",
-              ""
-            )
-
-            if arg == "" then
-              return { "${file}" }
-            end
-
-            table.insert(args, arg)
-
-            return args
-          end,
-          cwd = "${workspaceFolder}",
-          console = "integratedTerminal",
-        },
-        {
-          type = "pwa-chrome",
-          request = "launch",
-          name = "Chrome - launch",
-          runtimeExecutable = "/usr/bin/google-chrome",
-          runtimeArgs = {},
-          console = "integratedTerminal",
-          sourceMaps = true,
-          webRoot = ask_webroot,
-          remoteRoot = ask_remoteroot,
-          url = function()
-            ask_custom_command()
-
-            return ask_url()
-          end,
-        },
-        {
-          type = "pwa-chrome",
-          request = "attach",
-          name = "Chrome - attatch",
-          console = "integratedTerminal",
-          sourceMaps = true,
-          webRoot = ask_webroot,
-          remoteRoot = ask_remoteroot,
-          url = function()
-            ask_custom_command()
-
-            return ask_url()
-          end,
-          port = ask_port
-        },
-      }
-    end
-
-    dapui.setup({
-      expand_lines = false
-    })
-
-    dap.listeners.before.attach.dapui_config = function()
-      vim.notify("Debugger attatched", "info")
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      vim.notify("Debugger launched", "info")
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      vim.notify("Debugger teminated", "info")
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      vim.notify("Debugger exited", "info")
-    end
-  end
 }
 
 return M
