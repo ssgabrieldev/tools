4255e575f9d95aa4bb6ad7147986062eb14129e9
gabriel <ssgabrieldev@gmail.com>
2025-02-06 19:08:54 -0300

    new: nvim

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -2,7 +2,8 @@ return {
   "rcarriga/nvim-notify",
   opts = {
     background_colour = "#000000",
-    timeout = 500,
+    timeout = 1000,
+    stages = "slide",
     on_open = function(win)
       local buf = vim.api.nvim_win_get_buf(win)
       vim.api.nvim_buf_set_option(buf, "filetype", "notify")
