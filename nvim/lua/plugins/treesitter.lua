b674dd09624d7197bdea2afc12c6244fb63176eb
gabriel <ssgabrieldev@gmail.com>
2025-02-06 21:49:21 -0300

    new: nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -18,10 +18,5 @@ return {
     }
   },
   init = function()
-    vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    vim.opt.foldmethod = "expr"
-    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    vim.opt.foldenable = false
-    vim.opt.foldlevel = 99
   end
 }
