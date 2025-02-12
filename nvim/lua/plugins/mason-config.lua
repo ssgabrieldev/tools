789bf7ad5cdd4ebe38ce0329fbb634adbb7261bc
gabriel <ssgabrieldev@gmail.com>
2025-02-12 20:14:23 -0300

    new [nvim]: lsp signis off

--- nvim/lua/plugins/mason-config.lua
+++ nvim/lua/plugins/mason-config.lua
@@ -1,41 +1,75 @@
-local M = {
+return {
   "williamboman/mason-lspconfig.nvim",
   lazy = false,
   dependencies = {
-    "neovim/nvim-lspconfig"
+    {
+      "neovim/nvim-lspconfig",
+      -- opts = {
+      --   diagnostics = {
+      --     signs = {
+      --       text = {
+      --         [vim.diagnostic.severity.ERROR] = "",
+      --         [vim.diagnostic.severity.WARN] = "",
+      --         [vim.diagnostic.severity.HINT] = "",
+      --         [vim.diagnostic.severity.INFO] = "",
+      --       },
+      --       numhl = {
+      --         [vim.diagnostic.severity.WARN] = "WarningMsg",
+      --         [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+      --         [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+      --         [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+      --       },
+      --     },
+      --   }
+      -- }
+    }
   },
-}
-
-function M.config()
-  require("mason-lspconfig").setup({
+  opts = {
     ensure_installed = {
       "lua_ls",
       "ts_ls",
       "emmet_ls",
       "cssls"
     }
-  })
-  require("mason-lspconfig").setup_handlers({
-    function(server_name)
-      local utils_lsp = require('plugins.utils.lsp')
+  },
+  init = function()
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN] = '',
+          [vim.diagnostic.severity.INFO] = '',
+          [vim.diagnostic.severity.HINT] = '',
+        },
+        numhl = {
+          [vim.diagnostic.severity.WARN] = 'WarningMsg',
+          [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+          [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+          [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
 
-      require("lspconfig")[server_name].setup({
-        on_attach = utils_lsp.on_attach,
-        capabilities = utils_lsp.capabilities,
-        handlers = utils_lsp.handlers,
-      })
-    end,
-    ["emmet_ls"] = function(server_name)
-      local utils_lsp = require('plugins.utils.lsp')
+        },
+      },
+    })
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        local utils_lsp = require('plugins.utils.lsp')
 
-      require("lspconfig")[server_name].setup({
-        filetypes = { 'html', 'typescriptreact', 'javascriptreact' },
-        on_attach = utils_lsp.on_attach,
-        capabilities = utils_lsp.capabilities,
-        handlers = utils_lsp.handlers,
-      })
-    end
-  })
-end
+        require("lspconfig")[server_name].setup({
+          on_attach = utils_lsp.on_attach,
+          capabilities = utils_lsp.capabilities,
+          handlers = utils_lsp.handlers,
+        })
+      end,
+      ["emmet_ls"] = function(server_name)
+        local utils_lsp = require('plugins.utils.lsp')
 
-return M
+        require("lspconfig")[server_name].setup({
+          filetypes = { 'html', 'typescriptreact', 'javascriptreact' },
+          on_attach = utils_lsp.on_attach,
+          capabilities = utils_lsp.capabilities,
+          handlers = utils_lsp.handlers,
+        })
+      end
+    })
+  end
+}
