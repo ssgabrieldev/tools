7df9de97a385cffe9469b9d3a4f1d722c1f2e53d
gabriel <ssgabrieldev@gmail.com>
2025-05-20 11:53:34 -0300

    ref: nvim lualine theme

--- nvim/lua/lualine/themes/slipknot.lua
+++ nvim/lua/lualine/themes/slipknot.lua
@@ -1,5 +1,34 @@
 local theme = require("themes.slipknot")
 
+local function update_statusline_bg()
+  if vim.g.colors_name == target_colorscheme then
+    local mode = vim.fn.mode()
+    local bg_color = theme.colors.lualine_normal_bg.hex
+    local fg_color = theme.colors.lualine_normal_fg.hex
+
+    if mode:find('i') then
+      fg_color = theme.colors.lualine_insert_fg.hex
+      bg_color = theme.colors.lualine_insert_bg.hex
+    elseif mode:find('v') or mode:find('V') or mode == '' then
+      fg_color = theme.colors.lualine_visual_fg.hex
+      bg_color = theme.colors.lualine_visual_bg.hex
+    elseif mode:find('R') then
+      fg_color = theme.colors.lualine_replace_fg.hex
+      bg_color = theme.colors.lualine_replace_bg.hex
+    elseif mode:find('c') then
+      bg_color = theme.colors.lualine_command_bg.hex
+      fg_color = theme.colors.lualine_command_fg.hex
+    end
+
+
+    vim.api.nvim_set_hl(0, 'StatusLine', { bg = bg_color, fg = fg_color })
+  end
+end
+update_statusline_bg()
+vim.api.nvim_create_autocmd('ModeChanged', {
+  callback = update_statusline_bg,
+})
+
 return {
   normal = {
     a = { bg = theme.colors.lualine_normal_bg.hex, fg = theme.colors.lualine_normal_fg.hex },
