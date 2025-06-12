bff0f6c158d6b725950e2bbbdfbb42b99f536339
gabriel <ssgabrieldev@gmail.com>
2025-06-11 23:33:41 -0300

    fix: mason-config

--- nvim/lua/plugins/mason-config.lua
+++ nvim/lua/plugins/mason-config.lua
@@ -4,24 +4,6 @@ return {
   dependencies = {
     {
       "neovim/nvim-lspconfig",
-      -- opts = {
-      --   diagnostics = {
-      --     signs = {
-      --       text = {
-      --         [vim.diagnostic.severity.ERROR] = "",
-      --         [vim.diagnostic.severity.WARN] = "",
-      --         [vim.diagnostic.severity.HINT] = "",
-      --         [vim.diagnostic.severity.INFO] = "",
-      --       },
-      --       numhl = {
-      --         [vim.diagnostic.severity.WARN] = "WarningMsg",
-      --         [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-      --         [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-      --         [vim.diagnostic.severity.HINT] = "DiagnosticHint",
-      --       },
-      --     },
-      --   }
-      -- }
     }
   },
   opts = {
@@ -30,46 +12,67 @@ return {
       "ts_ls",
       "emmet_ls",
       "cssls"
-    }
+    },
+    automatic_enable = true
   },
   init = function()
+    local border = vim.g.border_style or "rounded"
+
+    local signs = {
+      Error = "",
+      Warn  = "",
+      Hint  = "󰌶",
+      Info  = "󰋽",
+    }
+
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
     vim.diagnostic.config({
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = '',
-          [vim.diagnostic.severity.WARN] = '',
-          [vim.diagnostic.severity.INFO] = '',
-          [vim.diagnostic.severity.HINT] = '',
-        },
-        numhl = {
-          [vim.diagnostic.severity.WARN] = 'WarningMsg',
-          [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-          [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
-          [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
-
-        },
-      },
+      float = { border = border },
+      signs = true,
     })
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        local utils_lsp = require('plugins.utils.lsp')
 
-        require("lspconfig")[server_name].setup({
-          on_attach = utils_lsp.on_attach,
-          capabilities = utils_lsp.capabilities,
-          handlers = utils_lsp.handlers,
-        })
+    require("lspconfig.ui.windows").default_options = {
+      border = border
+    }
+
+    local opts = { noremap = true, silent = true }
+
+    vim.lsp.config("*", {
+      handlers = {
+        ["textDocument/hover"] = vim.lsp.with(
+          vim.lsp.handlers.hover,
+          { border = border, max_width = 80, max_height = 40 }
+        ),
+        ["textDocument/signatureHelp"] = vim.lsp.with(
+          vim.lsp.handlers.signature_help,
+          { border = border }
+        ),
+      },
+
+      capabilities = vim.lsp.protocol.make_client_capabilities(),
+
+      on_attach = function(client, bufnr)
+        local bufopts = {
+          noremap = true,
+          silent = true,
+          buffer = bufnr,
+        }
+
+        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set({ "n", "i", "v" }, "<leader>lf", function()
+          vim.lsp.buf.format({ async = true })
+        end, bufopts)
+        vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>le", function()
+          vim.diagnostic.open_float()
+        end, opts)
       end,
-      ["emmet_ls"] = function(server_name)
-        local utils_lsp = require('plugins.utils.lsp')
-
-        require("lspconfig")[server_name].setup({
-          filetypes = { 'html', 'typescriptreact', 'javascriptreact' },
-          on_attach = utils_lsp.on_attach,
-          capabilities = utils_lsp.capabilities,
-          handlers = utils_lsp.handlers,
-        })
-      end
     })
   end
 }
