bc6f1fff369b4c5ea8007aa4862a391e63f3b910
gabriel <ssgabrieldev@gmail.com>
2025-07-06 07:08:13 -0300

    new: plugins

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,14 @@
+return {
+  "terrortylor/nvim-comment",
+  keys = {
+    {
+      "<leader>/",
+      ":CommentToggle<cr>",
+      desc = "Comment line",
+      mode = { "v", "n" }
+    },
+  },
+  config = function()
+    require('nvim_comment').setup()
+  end
+}
