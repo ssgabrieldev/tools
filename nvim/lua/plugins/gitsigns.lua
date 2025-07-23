acf342a47e8fb41f5ecf8ad8f975edc420dc4602
gabriel <ssgabrieldev@gmail.com>
2025-07-23 09:21:44 -0300

    new[nvim]: keymap to toggle git diff

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,11 +1,22 @@
-local M = {
+local vim = vim
+
+return {
   "lewis6991/gitsigns.nvim",
   event = "BufEnter",
   config = function()
     require('gitsigns').setup({
       current_line_blame = true
     })
-  end
+  end,
+  keys = {
+    {
+      "<leader>tg",
+      function()
+        vim.cmd("Gitsigns toggle_linehl")
+        vim.cmd("Gitsigns toggle_deleted")
+      end,
+      mode = { "n" },
+      { desc = "Toggle Git Diff" }
+    }
+  }
 }
-
-return M
