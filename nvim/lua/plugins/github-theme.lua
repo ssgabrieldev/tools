c0003641ad82368c45a3e4ad329a3f3ed3a843b6
gabriel <ssgabrieldev@gmail.com>
2025-11-24 11:29:16 -0300

    nvim: github theme transparent

--- nvim/lua/plugins/github-theme.lua
+++ nvim/lua/plugins/github-theme.lua
@@ -1,12 +1,12 @@
 return {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
-    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,
+    priority = 1000,
     config = function()
         require('github-theme').setup({
             options = {
-                transparent = false
+                transparent = true
             }
         })
 
