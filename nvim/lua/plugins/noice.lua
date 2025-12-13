70450c1d471a704f8e99d42a19c103542fafb000
gabriel <ssgabrieldev@gmail.com>
2025-12-13 15:14:08 -0300

    nvim: noice border

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,7 +1,11 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+      presets = {
+        lsp_doc_border = vim.g.border_style, -- add a border to hover docs and signature help
+      },
+  },
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
