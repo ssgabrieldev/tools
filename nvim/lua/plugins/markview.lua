ed424c093b3eb8433fb79315db6e0dd4eddebbf2
gabriel <ssgabrieldev@gmail.com>
2025-12-14 00:08:38 -0300

    ref: markview

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,10 +1,21 @@
+-- local presets = require("markview.presets").headings;
+
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  opts = {
-    preview = {
-      filetypes = { "markdown", "codecompanion" },
-      ignore_buftypes = {},
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+        preview = {
+            filetypes = { "markdown", "codecompanion" },
+            ignore_buftypes = {},
+        },
+        markdown = {
+            headings = {
+                shift_width = 0
+            },
+            code_blocks = {
+                wrap = false,
+                style = "simple"
+            }
+        }
     },
-  },
 }
