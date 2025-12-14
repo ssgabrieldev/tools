8a532e7685c483691b326e53cd3ff8e01b78f1af
gabriel <ssgabrieldev@gmail.com>
2025-12-13 23:26:03 -0300

    adf

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,13 +1,13 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-      presets = {
-        lsp_doc_border = vim.g.border_style, -- add a border to hover docs and signature help
-      },
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        presets = {
+            lsp_doc_border = vim.g.border_style,
+        },
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
     }
 }
