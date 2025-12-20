41cf51ccf3a37c09e6282c2a781722f550176acf
gabriel <ssgabrieldev@gmail.com>
2025-12-20 12:06:16 -0300

    ref: remove autocmd to dap-view buffers

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,16 +1,10 @@
 local dark_ns = vim.api.nvim_create_namespace("DarkWindowStyles")
-local nvim_tree_hl = vim.api.nvim_get_hl(0, { name = "NvimTreeNormal" })
 
 vim.api.nvim_set_hl(dark_ns, "Normal", { link = "NvimTreeNormal" })
 vim.api.nvim_set_hl(dark_ns, "CursorLine", { link = "Normal" })
 vim.api.nvim_set_hl(dark_ns, "EndOfBuffer", { link = "NvimTreeEndOfBuffer" })
-vim.api.nvim_set_hl(dark_ns, "NvimDapViewTab", { link = "NvimTreeNormal" })
 vim.api.nvim_set_hl(dark_ns, "WinBar", { link = "NvimTreeNormal" })
 vim.api.nvim_set_hl(dark_ns, "WinBarNC", { link = "NvimTreeNormal" })
-vim.api.nvim_set_hl(dark_ns, "NvimDapViewTabSelected", {
-    bold = true,
-    bg = nvim_tree_hl.bg
-})
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "codecompanion", "dap-repl", "dap-view", "qf" },
