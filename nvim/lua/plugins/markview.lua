5cae2cad0a9eb9737435e81748ea461fa6e7bc95
gabriel <ssgabrieldev@gmail.com>
2026-02-19 12:42:20 -0300

    nvim: markview code blocks

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -6,15 +6,32 @@ return {
     opts = {
         preview = {
             filetypes = { "markdown", "codecompanion" },
-            ignore_buftypes = {},
+            ignore_buftypes = { "nofile" }
         },
         markdown = {
             headings = {
-                shift_width = 0
+                shift_width = 0,
+                sign = false,
+                heading_1 = {
+                    sign = "",
+                },
+                heading_2 = {
+                    sign = "",
+                },
+                heading_3 = {
+                    sign = "",
+                },
+                heading_4 = {
+                    sign = "",
+                },
+                heading_5 = {
+                    sign = "",
+                }
             },
             code_blocks = {
-                wrap = false,
-                style = "simple"
+                sign = false,
+                style = "block",
+                label_direction = "right"
             }
         }
     },
