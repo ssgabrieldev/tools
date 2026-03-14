f0072050d1cd275cfa6def58ec62cc9668bc7da3
gabriel <ssgabrieldev@gmail.com>
2026-03-14 07:34:18 -0300

    wipe

--- check_temp/check_temp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-  cat /sys/class/thermal/thermal_zone*/temp
-  sleep 1
-done
--- fish/config.fish
+++
@@ -1,4 +0,0 @@
-starship init fish | source
-
-export PATH="$HOME/apps/bin:$PATH"
-export N_PREFIX="$HOME/apps/n"
